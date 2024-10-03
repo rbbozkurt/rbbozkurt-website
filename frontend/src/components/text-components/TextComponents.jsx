05806b2d7c8909168856ad59739ca49d6ab34d6f
rbbozkurt <resitberkaybozkurt@gmail.com>
2024-10-03 12:30:52 +0200

    homapage refactored for mobile screen sizes

--- frontend/src/components/text-components/TextComponents.jsx
+++ frontend/src/components/text-components/TextComponents.jsx
@@ -3,77 +3,130 @@ import { Typography, Box, Link, Divider } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 
-const HighlightedText = ({ text, sx }) => (
-    <Typography
-        variant="body1"
-        component="span"
-        sx={{ padding: '0 4px', fontStyle: 'italic', ...sx }}
-    >
-        {text}
-    </Typography>
-);
-
-const RoleText = ({ text, link, sx }) => (
-    <Typography
-        variant="body1"
-        component="span"
-        sx={{ padding: '0 4px', ...sx }}
-    >
-        {link ? (
-            <Link href={link} target="_blank" sx={{ textDecoration: 'none', color: 'inherit' }}>
-                {text}
-            </Link>
-        ) : (
-            text
-        )}
-    </Typography>
-);
-
-const Turkey = ({ text = "Turkey" }) => (
-    <HighlightedText
-        text={text}
-        sx={{ backgroundColor: 'red', color: 'white' }}
-    />
-);
-
-const Berlin = ({ text = "Berlin" }) => (
-    <HighlightedText
-        text={text}
-        sx={{
-            backgroundImage: 'linear-gradient(90deg, black 33%, red 33%, red 66%, gold 66%)',
-            color: 'white'
-        }}
-    />
-);
-
-const SubText = ({ text }) => (
-    <HighlightedText text={text} sx={{ fontStyle: 'italic' }} />
-);
-
-const Paragraph = ({ children }) => (
-    <Typography align="left" variant="body1" gutterBottom sx={{ padding: '0 0 16px 0' }}>
-        {children}
-    </Typography>
-);
-
-const Section = ({ title, children }) => (
-    <Box direction="column"
-        sx={{
-            padding: '64px 0 16px 0'
-        }}>
-        <Typography align="left" variant="h2">
-            {title}
-        </Typography>
-        <Divider
-            orientation='horizontal'
+const HighlightedText = ({ text, sx }) => {
+    const theme = useTheme();
+    return (
+        <Typography
+            variant="body1"
+            component="span"
             sx={{
-                backgroundColor: 'primary.light',
-                height: '1px', // Adjust the thickness here
-                margin: '4px 0 16px 0', // Add some space below the divider
-            }} />
-        {children}
-    </Box>
-);
+                padding: '0 4px',
+                fontStyle: 'italic',
+                fontSize: theme.typography.body1.fontSize,
+                color: theme.typography.body1.color,
+                ...sx
+            }}
+        >
+            {text}
+        </Typography>
+    );
+};
+
+const RoleText = ({ text, link, sx }) => {
+    const theme = useTheme();
+    return (
+        <Typography
+            variant="body1"
+            component="span"
+            sx={{
+                padding: '0 4px',
+                fontSize: theme.typography.body1.fontSize,
+                color: theme.typography.body1.color,
+                ...sx
+            }}
+        >
+            {link ? (
+                <Link href={link} target="_blank" sx={{ textDecoration: 'none', color: 'inherit' }}>
+                    {text}
+                </Link>
+            ) : (
+                text
+            )}
+        </Typography>
+    );
+};
+
+const Turkey = ({ text = "Turkey" }) => {
+    const theme = useTheme();
+    return (
+        <HighlightedText
+            text={text}
+            sx={{
+                backgroundColor: 'red',
+                color: 'white',
+                fontSize: theme.typography.body1.fontSize,
+            }}
+        />
+    );
+};
+
+const Berlin = ({ text = "Berlin" }) => {
+    const theme = useTheme();
+    return (
+        <HighlightedText
+            text={text}
+            sx={{
+                backgroundImage: 'linear-gradient(90deg, black 33%, red 33%, red 66%, gold 66%)',
+                color: 'white',
+                fontSize: theme.typography.body1.fontSize,
+            }}
+        />
+    );
+};
+
+const SubText = ({ text }) => {
+    const theme = useTheme();
+    return (
+        <HighlightedText
+            text={text}
+            sx={{
+                fontStyle: 'italic',
+                fontSize: theme.typography.body1.fontSize,
+                color: theme.typography.body1.color,
+            }}
+        />
+    );
+};
+
+const Paragraph = ({ children }) => {
+    const theme = useTheme();
+    return (
+        <Typography
+            align="left"
+            variant="body1"
+            gutterBottom
+            sx={{
+                padding: '0 0 16px 0',
+                fontSize: theme.typography.body1.fontSize,
+                color: theme.typography.body1.color,
+            }}
+        >
+            {children}
+        </Typography>
+    );
+};
+
+const Section = ({ title, children }) => {
+    const theme = useTheme();
+    return (
+        <Box direction="column"
+            sx={{
+                padding: '64px 0 16px 0'
+            }}>
+            <Typography align="left" variant="h2">
+                {title}
+            </Typography>
+            <Divider
+                orientation='horizontal'
+                sx={{
+                    backgroundColor: theme.palette.primary.light,
+                    height: '1px', // Adjust the thickness here
+                    margin: '4px 0 16px 0', // Add some space below the divider
+                }} />
+            {children}
+        </Box>
+    );
+};
 
 // this text is takes list of string and returns a string with # at the beginning of each item
 // uses RoleText for each list item and returns a Typography component
@@ -89,14 +142,11 @@ const colorMap = {
     testing: { color: '#FF5722', backgroundColor: '#FFEBEE' }, // Orange for testing
 };
 
-
 const getColor = (index) => {
     const keys = Object.keys(colorMap);
     const key = keys[index % keys.length];
     return colorMap[key];
-}
-
-
+};
 
 const Tags = ({ tags, isColorized = true, isHorizontalScrollable = false }) => {
     const theme = useTheme();
@@ -127,6 +177,7 @@ const Tags = ({ tags, isColorized = true, isHorizontalScrollable = false }) => {
                             color: isColorized ? color : theme.palette.secondary.dark,
                             backgroundColor: isColorized ? backgroundColor : '#E5E5E5',
                             padding: '1px 4px',
+                            fontSize: theme.typography.body1.fontSize,
                         }}
                     >
                         {tag}
@@ -144,6 +195,5 @@ Tags.propTypes = {
 };
 
 export default Tags;
-
 
 export { Section, Paragraph, RoleText, Turkey, Berlin, SubText, Tags };
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,5 +1,72 @@
 import { createTheme } from '@mui/material/styles';
 
+const commonTheme = {
+    typography: {
+        fontFamily: '"Source Code Pro", monospace',
+        h1: {
+            fontWeight: 700,
+            fontSize: { xs: '2rem', sm: '2.5rem', md: '3rem' },
+            color: 'rgb(21, 21, 21)',
+        },
+        h2: {
+            fontWeight: 600,
+            fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem' },
+            color: '#3d3d3d',
+        },
+        h3: {
+            fontWeight: 600,
+            fontSize: { xs: '1.2rem', sm: '1.5rem', md: '2rem' },
+            color: '#6b6b6b',
+        },
+        h4: {
+            fontFamily: '"Playwrite CU", cursive',
+            fontWeight: 400,
+            fontSize: { xs: '1rem', sm: '1.2rem', md: '1.5rem' },
+            color: '#fefefe',
+        },
+        body1: {
+            fontSize: { xs: '0.8rem', sm: '1rem', md: '1.2rem' },
+            color: '#000',
+        },
+        body2: {
+            fontSize: { xs: '0.7rem', sm: '0.8rem', md: '0.9rem' },
+            color: '#3d3d3d',
+        },
+        button: {
+            fontSize: { xs: '0.8rem', sm: '1rem', md: '1.2rem' },
+            textTransform: 'none',
+            color: '#fefefe',
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: '#FFD700',
+                    color: '#fefefe',
+                    '&:hover': {
+                        backgroundColor: '#FFD700',
+                    },
+                },
+            },
+        },
+        MuiCircularProgress: {
+            styleOverrides: {
+                colorPrimary: {
+                    color: '#FFD700',
+                },
+            },
+        },
+        MuiAppBar: {
+            styleOverrides: {
+                colorPrimary: {
+                    backgroundColor: 'rgb(21, 21, 21)',
+                },
+            },
+        },
+    },
+};
+
 const lightMode = {
     palette: {
         mode: 'light',
@@ -49,7 +116,10 @@ const lightMode = {
                 hoverColor: '#FFC107',
             },
             darkModeToggle: {
-                color: '#2D3242',
+                color: {
+                    light: '#2D3242',
+                    dark: '#FB940B',
+                },
                 transition: 'transform 0.5s',
             },
             socialMedia: {
@@ -108,78 +178,14 @@ const darkMode = {
                 hoverColor: '#FFC107',
             },
             darkModeToggle: {
-                color: '#FB940B',
+                color: {
+                    light: '#2D3242',
+                    dark: '#FB940B',
+                },
                 transition: 'transform 0.5s',
             },
             socialMedia: {
                 color: 'primary.light',
-            },
-        },
-    },
-};
-
-const commonTheme = {
-    typography: {
-        fontFamily: '"Source Code Pro", monospace',
-        h1: {
-            fontWeight: 700,
-            fontSize: '2.5rem',
-            color: 'rgb(21, 21, 21)',
-        },
-        h2: {
-            fontWeight: 600,
-            fontSize: '2rem',
-            color: '#3d3d3d',
-        },
-        h3: {
-            fontWeight: 600,
-            fontSize: '2rem',
-            color: '#6b6b6b',
-        },
-        h4: {
-            fontFamily: '"Playwrite CU", cursive',
-            fontWeight: 400,
-            fontSize: '1.5rem',
-            color: '#fefefe',
-        },
-        body1: {
-            fontSize: '1rem',
-            color: '#000',
-        },
-        body2: {
-            fontSize: '0.9rem',
-            color: '#3d3d3d',
-        },
-        button: {
-            fontSize: '1rem',
-            textTransform: 'none',
-            color: '#fefefe',
-        },
-    },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: '#FFD700',
-                    color: '#fefefe',
-                    '&:hover': {
-                        backgroundColor: '#FFD700',
-                    },
-                },
-            },
-        },
-        MuiCircularProgress: {
-            styleOverrides: {
-                colorPrimary: {
-                    color: '#FFD700',
-                },
-            },
-        },
-        MuiAppBar: {
-            styleOverrides: {
-                colorPrimary: {
-                    backgroundColor: 'rgb(21, 21, 21)',
-                },
             },
         },
     },
